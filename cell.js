156c3b6347edc6b537de1d5f99029451ea7c7a27
Ryan Webb <rywebb1998@gmail.com>
2024-02-25 10:29:47 +0000

    traverse the grid

--- cell.js
+++ cell.js
@@ -1,8 +1,15 @@
 class Cell {
-	static cellSize = 40;
+	/**
+	 * All the cells
+	 * @type {Cell[]}
+	 */
+	static Cells = [];
+	static size = 40;
 
 	rowIdx;
 	colIdx;
+
+	visited = false;
 
 	drawTopWall = true;
 	drawRightWall = true;
@@ -10,33 +17,25 @@ class Cell {
 	drawLeftWall = true;
 
 	/**
-	 * Create a new cell
+	 * Create a new cell, and push it to the Cell.Cells array
 	 * @param {number} rowIdx The index of the row (0 being top)
 	 * @param {number} colIdx The index of the col (0 being left)
 	 */
-	constructor(colIdx, rowIdx) {
+	constructor(rowIdx, colIdx) {
 		this.rowIdx = rowIdx;
 		this.colIdx = colIdx;
-	}
 
-	/**
-	 * Draw all the cell walls
-	 */
-	resetWalls() {
-		this.drawTopWall = true;
-		this.drawRightWall = true;
-		this.drawBottomWall = true;
-		this.drawLeftWall = true;
+		Cell.Cells.push(this);
 	}
 
 	/**
 	 * Draw the cell
 	 */
 	draw() {
-		const left = this.colIdx * Cell.cellSize;
-		const right = (this.colIdx + 1) * Cell.cellSize;
-		const top = this.rowIdx * Cell.cellSize;
-		const bottom = (this.rowIdx + 1) * Cell.cellSize;
+		const top = this.rowIdx * Cell.size;
+		const left = this.colIdx * Cell.size;
+		const bottom = (this.rowIdx + 1) * Cell.size;
+		const right = (this.colIdx + 1) * Cell.size;
 
 		stroke(255);
 
@@ -56,7 +55,52 @@ class Cell {
 			line(left, bottom, left, top);
 		}
 
-		// noFill();
-		// rect(x, y, Cell.cellSize, Cell.cellSize);
+		if (this.visited) {
+			noStroke();
+			fill(255, 0, 255, 100);
+			rect(left, top, Cell.size, Cell.size);
+		}
+	}
+
+	/**
+	 * Get a cell at a certain row and column
+	 * Returns null, if that cell doesn't exist
+	 * @param {number} row
+	 * @param {number} col
+	 * @returns {Cell | null} The cell at the row and column, or null
+	 */
+	static getCell(row, col) {
+		const colCount = Math.floor(width / Cell.size);
+		const rowCount = Math.floor(height / Cell.size);
+
+		if (row < 0 || row > rowCount - 1) return null;
+		if (col < 0 || col > colCount - 1) return null;
+
+		return Cell.Cells[col + row * colCount];
+	}
+
+	/**
+	 * Get a random next neighbour that hasn't been visited
+	 * Returns null if there are no unvisited neighbours
+	 * @returns {Cell | null} Next cell that hasn't been visited, or null if none available
+	 */
+	getNextNeighbour() {
+		const neighbours = [
+			// top
+			Cell.getCell(this.rowIdx - 1, this.colIdx),
+			// right
+			Cell.getCell(this.rowIdx, this.colIdx + 1),
+			// bottom
+			Cell.getCell(this.rowIdx + 1, this.colIdx),
+			// left
+			Cell.getCell(this.rowIdx, this.colIdx - 1)
+		].filter(c => c?.visited === false);
+
+		if (neighbours.length) {
+			const idx = Math.floor(random(0, neighbours.length));
+			return neighbours[idx];
+		}
+
+		return null;
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,22 +1,46 @@
-let colCount, rowCount;
-const cells = [];
+/**
+ * The number of columns to draw
+ * @type {number}
+ */
+let colCount;
+/**
+ * The number of rows to draw
+ * @type {number}
+ */
+let rowCount;
+/**
+ * The current cell being checked
+ * @type {Cell}
+ */
+let currentCell;
 
 function setup() {
+	frameRate(5);
+
 	createCanvas(400, 400);
-	colCount = Math.floor(width / Cell.cellSize);
-	rowCount = Math.floor(height / Cell.cellSize);
+	colCount = Math.floor(width / Cell.size);
+	rowCount = Math.floor(height / Cell.size);
 
 	// create all the cells
 	for (let row = 0; row < rowCount; row++) {
 		for (let col = 0; col < colCount; col++) {
-			cells.push(new Cell(row, col));
+			new Cell(row, col);
 		}
 	}
+
+	currentCell = Cell.Cells[0];
+	currentCell.visited = true;
 }
 
 function draw() {
 	background(51);
 
+	const nextCell = currentCell.getNextNeighbour();
+	if (nextCell) {
+		currentCell = nextCell;
+		currentCell.visited = true;
+	}
+
 	// draw all the cells
-	cells.forEach(c => c.draw());
+	Cell.Cells.forEach(c => c.draw());
 }
